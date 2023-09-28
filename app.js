267bd96417e39326152cfde033aa4bf903939ab4
Ryan Li <jijizaza999@gmail.com>
2023-09-28 15:48:38 -0700

    resubmit

--- app.js
+++ app.js
@@ -58,83 +58,22 @@ console.log(reverseWords(sentence));
 console.log(reverseWords("My name is Ryan"));
 console.log(reverseWords("My dogs name is LuLu"));
 
-// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
 
+// Exercise 4 Section
 let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 
-function csvConverter(data) {
-    let headers =data.slice(0, data.indexOf("\n")).split(",");
+let parse = csvData.split("\n");
+let headers = parse[0].split(",");
+const dataTable = [];
 
-    let valuesList = data.slice(data.indexOf("\n")+ 1).split("\n");
+for (i = 1; i < parse.length; i++){
+    const rowArray = parse[i].split(",");
+    const rowObject = {
+        name: rowArray[0],
+        age: rowArray[1],        
+    };
+    dataTable.push(rowObject);
+};
 
-    const output = valuesList.map((row) => {
-        let values = row.split(",");
-        let obj = {};
-
-        values.forEach((value, idx) => {
-            if (idx < headers.length) {
-                obj[headers[idx]] = values;
-            } else {
-                obj[`misc${idx- headers.length + 1}`] = values;
-            }
-        });
-
-        return obj;
-    });
-
-    return output;
-}
-console.log (csvConverter(csvData));
-console.log(
-    csvConverter("name,city,occupation\nJake,St. Petersburg, Student, Some Value")
-);
-
-function converter(fileData) {
-
-    let rows = fileData.split("\n");
-    let headers = rows[0].split(",");
-
-    for (let i =1; i < rows.length; i++) {
-        let obj ={};
-        let data = rows[i].split(",");
-        data.forEach((value,index) => {
-            let headerName = headers[index];
-            obj[headerName] = value;
-        });
-        result.push(obj);
-    }
-    return result;
-}
-
-console.log(converter(csvData));
-
-function reduceConvert(fileData) {
-
-    return fileData.split("\n").reduce((output, row, index, arr) =>{
-        let values = row.split(",")
-        if (index == 0) {
-            output.headers = values;
-        } else {
-            let obj = {};
-            values.forEach((val,index)=>{
-                if (index < output.headers.length){
-                    obj[headers[index]] = val;
-                } else {
-                    obj[`misc${index -output.headers.length + 1}`]= val;
-                }
-            });
-
-            if (!output.records) {
-                output.records = [];
-            }
-
-            output.records.push(obj);
-        }
-        return output;
-    },{});
-
-    console.log
-
-
-}
-
+console.log(dataTable);
